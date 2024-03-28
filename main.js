06f65ab2047cb6e4f707a75f09efb31d59d14922
Rinaldots <rinaldotavares12@gmail.com>
2024-03-28 11:18:45 -0300

    Add files via upload
    Codigo de Alan

---
+++ main.js
@@ -0,0 +1,147 @@
+let scene, camera, rendered, cube;
+let theta1=0
+let theta2=0
+let theta3=0
+
+let dthetaXdt=0;
+let dthetaYdt=0;
+let dthetaZdt=0;
+
+let dtheta1dt=0;
+let dtheta2dt=0;
+let dtheta3dt=0;
+
+let thetaX=0;
+let thetaY=0;
+let thetaZ=0;
+
+let det=false
+
+let t=0,dt=1;
+
+function cb(event){
+  dthetaXdt=event.rotationRate.alpha*PI/180;
+  dthetaYdt=event.rotationRate.beta*PI/180;
+  dthetaZdt=event.rotationRate.gamma*PI/180;
+  
+  let ct=millis()/1000;
+  dt=ct-t;
+  t=ct;
+
+  det=true;
+}
+
+function onClick(){
+  if (typeof DeviceMotionEvent.requestPermission === 'function') {
+    DeviceMotionEvent.requestPermission()
+      .then(permissionState => {
+        if (permissionState === 'granted') {
+          window.addEventListener('devicemotion', cb);
+        }
+      })
+      .catch(console.error);
+  } else {
+    window.addEventListener('devicemotion', cb);
+    // handle regular non iOS 13+ devices
+  }
+}
+
+
+
+function parentWidth(elem) {
+  return elem.parentElement.clientWidth;
+}
+
+function parentHeight(elem) {
+  return elem.parentElement.clientHeight;
+}
+
+function init3D(){
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xffffff);
+
+  camera = new THREE.PerspectiveCamera(75, parentWidth(document.getElementById("3Dcube")) / parentHeight(document.getElementById("3Dcube")), 0.1, 1000);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(parentWidth(document.getElementById("3Dcube")), parentHeight(document.getElementById("3Dcube")));
+
+  document.getElementById('3Dcube').appendChild(renderer.domElement);
+
+  // Create a geometry
+  const geometry = new THREE.BoxGeometry(5, 1, 4);
+
+  // Materials of each face
+  var cubeMaterials = [
+    new THREE.MeshBasicMaterial({color:0x03045e}),
+    new THREE.MeshBasicMaterial({color:0x023e8a}),
+    new THREE.MeshBasicMaterial({color:0x0077b6}),
+    new THREE.MeshBasicMaterial({color:0x03045e}),
+    new THREE.MeshBasicMaterial({color:0x023e8a}),
+    new THREE.MeshBasicMaterial({color:0x0077b6}),
+  ];
+
+  const material = new THREE.MeshFaceMaterial(cubeMaterials);
+
+  cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+  camera.position.z = 5;
+  renderer.render(scene, camera);
+}
+
+// Resize the 3D object when the browser window changes size
+function onWindowResize(){
+  camera.aspect = parentWidth(document.getElementById("3Dcube")) / parentHeight(document.getElementById("3Dcube"));
+  //camera.aspect = window.innerWidth /  window.innerHeight;
+  camera.updateProjectionMatrix();
+  //renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(parentWidth(document.getElementById("3Dcube")), parentHeight(document.getElementById("3Dcube")));
+
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+// Create the 3D representation
+init3D();
+
+// Create events for the sensor readings
+if (!!window.EventSource) {
+  var source = new EventSource('/events');
+
+  source.addEventListener('open', function(e) {
+    console.log("Events Connected");
+  }, false);
+
+  source.addEventListener('error', function(e) {
+    if (e.target.readyState != EventSource.OPEN) {
+      console.log("Events Disconnected");
+    }
+  }, false);
+
+  source.addEventListener('gyro_readings', function(e) {
+    //console.log("gyro_readings", e.data);
+    var obj = JSON.parse(e.data);
+    document.getElementById("gyroX").innerHTML = obj.dthetaXdt;
+    document.getElementById("gyroY").innerHTML = obj.dthetaYdt;
+    document.getElementById("gyroZ").innerHTML = obj.dthetaZdt;
+
+    // Change cube rotation after receiving the readinds
+    cube.rotation.x = obj.dthetaXdt;
+    cube.rotation.z = obj.dthetaYdt;
+    cube.rotation.y = obj.dthetaZdt;
+    renderer.render(scene, camera);
+  }, false);
+}
+
+function resetPosition(element){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/"+element.id, true);
+  console.log(element.id);
+  xhr.send();
+}
+
+const coreografias = getElementById('coreografia');
+
+const coreoInput = document.createElement('input');
+
+ 
+
