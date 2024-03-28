21ca72be209e488775712aa7f805e852930be919
Rinaldots <rinaldotavares12@gmail.com>
2024-03-28 12:51:25 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,56 @@
 const gyroData = {
-      x: null,
-      y: null,
-      z: null,
-      alpha: null,
-      beta: null,
-      gamma: null
-    };
+  x: null,
+  y: null,
+  z: null,
+  alpha: null,
+  beta: null,
+  gamma: null,
+  dalpha: null,
+  dbeta: null,
+  dgamma: null
+};
+const temp = {
+  alpha: null,
+  beta: null,
+  gamma: null
+}
 
-    function onClick() {
-      if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-        DeviceOrientationEvent.requestPermission()
-          .then(permissionState => {
-            if (permissionState === 'granted') {
-              window.addEventListener('deviceorientation', cb);
-            }
-          })
-          .catch(console.error);
-      } else {
-        window.addEventListener('deviceorientation', cb);
-      }
-    }
+function onClick() {
+  if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+    DeviceOrientationEvent.requestPermission()
+      .then(permissionState => {
+        if (permissionState === 'granted') {
+          window.addEventListener('deviceorientation', cb);
+        }
+      })
+      .catch(console.error);
+  } else {
+    window.addEventListener('deviceorientation', cb);
+  }
+}
 
-    function cb(event) {
-      gyroData.alpha = event.alpha;
-      gyroData.beta = event.beta;
-      gyroData.gamma = event.gamma;
+function cb(event) {
+  temp.alpha = gyroData.alpha;
+  temp.beta = gyroData.beta;
+  temp.gamma = gyroData.gamma;
 
-      
-      document.getElementById('gyroX').textContent = gyroData.alpha.toFixed(3);
-      document.getElementById('gyroY').textContent = gyroData.beta.toFixed(3);
-      document.getElementById('gyroZ').textContent = gyroData.gamma.toFixed(3);
-    }
+  gyroData.alpha = event.alpha;
+  gyroData.beta = event.beta;
+  gyroData.gamma = event.gamma;
 
-    let q; // Declare a variável q
+  
+  gyroData.dalpha = gyroData.alpha - temp.alpha;
+  gyroData.dbeta = gyroData.beta - temp.beta;
+  gyroData.dgamma = gyroData.gamma - temp.gamma;
 
+  update();
+}
+function update(){
+  document.getElementById('gyroAlpha').textContent = gyroData.alpha.toFixed(3);
+  document.getElementById('gyroBeta').textContent = gyroData.beta.toFixed(3);
+  document.getElementById('gyroGamma').textContent = gyroData.gamma.toFixed(3);
+  document.getElementById('DgyroAlpha').textContent = gyroData.dalpha.toFixed(3);
+  document.getElementById('DgyroBeta').textContent = gyroData.dbeta.toFixed(3);
+  document.getElementById('DgyroGamma').textContent = gyroData.dgamma.toFixed(3);
+}
 
-    
